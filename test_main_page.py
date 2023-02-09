9247d6c66f182d1573ae3284519f924fdb2022b8
Stas_Litvinov <staslitvinovqa@mail.com>
2023-02-09 23:13:06 +0400

    Created methods in login_page.py, added locators and tests in files

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,39 @@
+import pytest
+
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
-
+@pytest.mark.main_page
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.main_page
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+@pytest.mark.login_page
+def test_guest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+@pytest.mark.login_page
+def test_guest_should_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+@pytest.mark.login_page
+def test_login_string_should_be_in_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
